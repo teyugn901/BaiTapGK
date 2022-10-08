faeffc936b1426a3b00d4142481a502d69c2e6c7
teyugn901 <thaonguyet02vn@gmail.com>
2022-10-08 18:39:16 +0700

    SignUp _ Profile

---
+++ app/src/main/java/com/example/btgk/SignInActivity.java
@@ -0,0 +1,14 @@
+package com.example.btgk;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+public class SignInActivity extends MainActivity{
+
+
+
+}
---
+++ app/src/main/java/com/example/btgk/SignUpActivity.java
@@ -0,0 +1,26 @@
+package com.example.btgk;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
